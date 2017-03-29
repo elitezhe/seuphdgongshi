985c429ba402b6c3e8a5a35d2fbb2a47c045306a
Zhang Zhe <zhangzhe.zhe@qq.com>
2017-03-29 20:26:10 +0800

    类框架基本形成,但未实现具体功能

--- xueweiinfo.py
+++ xueweiinfo.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
+from bs4 import BeautifulSoup
 
 
 class XueweiInfo(object):
-    def __init__(self):
-        self.xh = ''  # 学号
+    def __init__(self, xuehao):
+        self.xh = xuehao  # 学号
         self.raw = u''  # HTML page
         self.lx = u''  # 类型
         self.ydbrq = u''  # 预答辩日期
@@ -28,9 +29,79 @@ class XueweiInfo(object):
 
     def get_raw(self):
         """
-
+        获取包含答辩信息的HTML页面源码
         :return:
         """
         xw_url = ur'http://202.119.4.150/newyjsy/byyxwgl/bssdbxxgsdetail.aspx?xh=%sxw' % self.xh
+        request = urllib2.Request(xw_url)
+        response_data = urllib2.urlopen(request)
+        response = response_data.read().decode('gb2312').encode('utf-8')
+        self.raw = response
+
+    def translate_raw(self):
+        """
+        将HTML页面中信息提取并保存
+        :return:
+        """
+        # todo 判断raw是否成功获取
+
+        soup = BeautifulSoup(self.raw)
+        self.lx = soup.find(id='lbllx').get_text(strip=True)
+        self.ydbrq = soup.find(id='lbldbrq').get_text(strip=True)
+        # todo 补全其他
+
+    def save_to_database(self):
+        pass
+
+    def read_from_database(self):
+        pass
 
 
+class Xstl(object):
+    """学术讨论"""
+    def __init__(self):
+        # 主办单位 时间 地点 报告人 报告主题
+        self.zbdw = u''
+        self.sj = u''
+        self.dd = u''
+        self.bgr = u''
+        self.bgzt = u''
+
+    def set_from_list(self, infolist):
+        pass
+
+    def get_printable(self):
+        pass
+
+
+class Xshy(object):
+    """学术会议"""
+    def __init__(self):
+        # 会议名称 时间 地点 本人报告 本人报告题目
+        self.hymc = u''
+        self.sj = u''
+        self.dd = u''
+        self.brbg = u''
+        self.bgtm = u''
+
+    def set_from_list(self, infolist):
+        self.hymc = ''
+
+
+class Dbwy(object):
+    """答辩委员会和答辩秘书信息"""
+    def __init__(self):
+        # 姓名 职称 导师类别 工作单位 是否主席 备注
+        self.xm = u''
+        self.zc = u''
+        self.dslb = u''
+        self.gzdw = u''
+        self.sfzx = u''
+        self.bz = u''
+
+    def set_from_list(self, infolist):
+        pass
+
+
+if __name__ == '__main__':
+    pass
