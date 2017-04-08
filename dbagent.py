9def6e309f68e38026667bf9844f11345ca3ca9a
Zhang Zhe <zhangzhe.zhe@qq.com>
2017-04-08 22:11:22 +0800

    小改动

--- dbagent.py
+++ dbagent.py
@@ -5,14 +5,15 @@ from logger import Logger
 
 
 class DbAgent(object):
-    def __init__(self, dbfile='xueweidb'):
-        if dbfile == 'xueweidb':
-            curr_path = os.path.curdir
-            db_path = os.path.join(curr_path, dbfile)
+    def __init__(self, dbfile='xueweidb.db'):
+        if dbfile == 'xueweidb.db':
+            data_path = os.path.join(os.path.curdir, 'data')
+            db_path = os.path.join(data_path, dbfile)
         else:
             db_path = dbfile
         self.conn = sqlite3.connect(db_path)
         self.cur = self.conn.cursor()
+        self.table = 'xuewei'
 
     def commit(self):
         self.conn.commit()
@@ -23,5 +24,9 @@ class DbAgent(object):
     def write_student(self, student):
         pass
 
+    def read_student(self):
+        pass
+
 if __name__ == '__main__':
-    pass
+    dba = DbAgent('.\data\SEUStudents.sqlite')
+
