11172f31611760619d57027d07e05aafb6853e5d
Zhang Zhe <zhangzhe.zhe@qq.com>
2017-04-09 10:42:04 +0800

    加入返回未抓取列表信息的函数.

--- dbagent.py
+++ dbagent.py
@@ -21,12 +21,35 @@ class DbAgent(object):
     def close(self):
         self.conn.close()
 
+    def get_uncrawled_student_list(self):
+        """返回学号(string)的列表 -- list(string)"""
+        sql = r'SELECT XH FROM xuewei WHERE HAVEDATA == 0'
+        self.cur.execute(sql)
+        result = self.cur.fetchall()
+        stu_list = []
+        if len(result) > 0:
+            for res in result:
+                stu_list.append(res[0])
+            stu_list.append('-1')
+            return stu_list
+        else:
+            return ['-1']
+
     def write_student(self, student):
-        pass
+        xh = student.get_xh()
+        if xh == '-1':
+            return
+        sql = student.save_to_database()
+        self.cur.execute(sql)
+
+        sql2 = "UPDATE xuewei SET HAVEDATA = 1 WHERE XH = '%s'" % xh
+        self.cur.execute(sql2)
 
     def read_student(self):
         pass
 
 if __name__ == '__main__':
     dba = DbAgent('.\data\SEUStudents.sqlite')
+    stu_list = dba.get_uncrawled_student_list()
+    print(stu_list)
 
